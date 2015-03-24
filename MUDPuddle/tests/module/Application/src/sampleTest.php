fe78391dd1d6e3fe950801095d3579117292589d
Derek Ling <galatoni@gmail.com>
2015-03-24 20:39:33 +0000

    Adds phpunit to composer.json, adds dir tree
    Phpunit is in, TDD all the way!

---
+++ MUDPuddle/tests/module/Application/src/sampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class sampleTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function sample_test_will_return_true()
+    {
+        $this->assertTrue(true);
+    }
+}
